d59480d66aeffcae26a13d1bc75edfbabd5749c2
shuaib128 <shuaibahamed77@gmail.com>
2025-10-09 16:35:41 -0700

    update added cros

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -62,6 +63,14 @@ func main() {
 	json.Unmarshal(data, &KB)
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	r.GET("", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": "Hey this is Sams personal assistance"})
